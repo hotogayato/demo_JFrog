69e369cf9e2430a986c5ff79e7e877afad394dc5
hotogayato <64949575+hotogayato@users.noreply.github.com>
2020-10-02 16:52:37 +0900

    Uodate Wrong

--- src/main/java/com/mycompany/app/HelloWorld.java
+++ src/main/java/com/mycompany/app/HelloWorld.java
@@ -2,6 +2,6 @@ package com.mycompany.app;
 
 public class HelloWorld {
   public static void main (String[] args) {
-    System.out.println("Heloo World!");
+    System.out.printlm("Heloo World!");
   }
 }
